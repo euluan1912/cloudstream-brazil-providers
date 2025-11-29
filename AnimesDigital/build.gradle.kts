9bf58e196fe640debee46c4381bc003c5ec29dcf
euluan1912 <euluan1912@gmail.com>
2025-11-29 06:26:34 -0300

    Atualizar o build.gradle.kts

--- AnimesDigital/build.gradle.kts
+++ AnimesDigital/build.gradle.kts
@@ -3,7 +3,7 @@ version = 2
 cloudstream {
     description = "Animes Digital - Animes em FHD LEG e DUB."
     language = "pt-br"
-    authors = listOf("saimuelbr", "Sephiroth") 
+    authors = listOf("euluan1912", "Sephiroth") 
     status = 1
     tvTypes = listOf("Anime", "AnimeMovie") 
     iconUrl = "https://animesdigital.org/wp-content/uploads/2025/03/cropped-logo-e1740827992823-192x192.png"
