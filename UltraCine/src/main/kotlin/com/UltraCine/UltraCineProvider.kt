dd1331a70c18b03e487baa3fb7137d8a70f0ead1
L <euluan1912@gmail.com>
2025-12-06 09:36:42 -0300

    Atualizar o UltraCineProvider.kt

--- UltraCine/src/main/kotlin/com/UltraCine/UltraCineProvider.kt
+++ UltraCine/src/main/kotlin/com/UltraCine/UltraCineProvider.kt
@@ -2,24 +2,10 @@ package com.UltraCine
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-// ADICIONE AS IMPORTAÇÕES NECESSÁRIAS AQUI:
-import com.lagradost.cloudstream3.extractors.ExtractorApi
-import com.lagradost.cloudstream3.MainAPI
 
-// Importa as classes de extratores que você criou no mesmo pacote
-import com.UltraCine.PlayEmbedApiSite // Certifique-se que o nome do arquivo/classe é este
-import com.UltraCine.EmbedPlayExtractor // Certifique-se que o nome do arquivo/classe é este
-
-@CloudstreamPlugin
-class UltraCineProvider : BasePlugin() {
-    override fun load() {
-        // CORRIGE UNRESOLVED REFERENCE PARA UltraCine
-        registerMainAPI(UltraCine()) 
-        
-        // CORRIGE UNRESOLVED REFERENCE PARA PlayEmbedApiSite
-        registerExtractorAPI(PlayEmbedApiSite())
-        
-        // CORRIGE UNRESOLVED REFERENCE PARA EmbedPlayExtractor
-        registerExtractorAPI(EmbedPlayExtractor()) 
-    } // <--- CHAVE DE FECHAMENTO CORRETA PARA O override fun load()
-} // <--- CHAVE DE FECHAMENTO CORRETA PARA A CLASSE UltraCineProvider
+// Importa as classes que estão no mesmo pacote (com.UltraCine)
+// Se PlayEmbedApiSite e EmbedPlayExtractor estão no mesmo diretório, 
+// o import completo 'com.UltraCine.NomeDaClasse' pode ser necessário, 
+// ou apenas use o nome da classe, dependendo da configuração.
+import com.UltraCine.PlayEmbedApiSite
+import com.UltraCine.EmbedPlayExtractor
