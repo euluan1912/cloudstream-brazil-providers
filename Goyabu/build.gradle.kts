92adff85edeae47736817e084ef5c8206704e6c3
L <euluan1912@gmail.com>
2025-12-29 10:49:55 -0300

    Goyabu V1
    build do goyabu adicionado.
    
    Signed-off-by: L <euluan1912@gmail.com>

---
+++ Goyabu/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Animes Online em HD gratuitamente no Goyabu"
+    language = "pt-br"
+    authors = listOf("lawlietbr")
+    status = 1
+    tvTypes = listOf("Animes")
+    iconUrl = "https://goyabu.io/wp-content/uploads/2025/12/cropped-goyabu-favicon-192x192.png"
+    isCrossPlatform = true
+}
