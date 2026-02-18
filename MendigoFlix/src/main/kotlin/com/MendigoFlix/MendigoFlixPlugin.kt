69f28c5024b695a8d4d71af11fc581838f74e162
L <euluan1912@gmail.com>
2026-02-18 14:38:08 -0300

    MendigoFlix V3
    Mudança de domínio do antigo SuperFlix.
    
    Signed-off-by: L <euluan1912@gmail.com>

--- MendigoFlix/src/main/kotlin/com/MendigoFlix/MendigoFlixPlugin.kt
+++ MendigoFlix/src/main/kotlin/com/MendigoFlix/MendigoFlixPlugin.kt
@@ -1,12 +1,12 @@
-package com.SuperFlix
+package com.MendigoFlix
 
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class SuperFlixProvider : Plugin() {
+class MendigoFlixProvider : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(SuperFlix())
+        registerMainAPI(MendigoFlix())
     }
 }
